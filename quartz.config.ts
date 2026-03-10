5048987e95b12c143673a511fbb984d1bcf58212
mr2dx2 <bidimosu4@email.com>
2026-03-10 06:56:49 -0300

    deploy setup

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "trulythebesthackerever.github.io/quartz",
+    baseUrl: "trulythebesthackerever/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
