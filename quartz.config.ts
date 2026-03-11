c820b861dfe87aa361f2b1f83cc19ebf92db591c
dockertor <momonitrium@gmail.com>
2026-03-10 21:44:38 -0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "trulythebesthackerever/quartz",
+    baseUrl: "dockertor.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
